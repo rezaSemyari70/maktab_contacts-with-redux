caf59c4065ed4db01cfad711110f291015561e5b
reza <rezasemyari1991@gmail.com>
2020-08-18 10:53:57 +0430

    Updates

---
+++ precache-manifest.38600dbb2f8e327fb428a5026ae73924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3dba1173078d46fc3840f9c883a297b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd762fa5a2ce6593416b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd762fa5a2ce6593416b",
+    "url": "/static/js/main.c9f73b74.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
